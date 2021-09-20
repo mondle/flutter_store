f0f4197ae64ac7bc66d3fe9b497777f0ec2b0e6c
mondle <einrede@hotmail.com>
2021-09-20 21:47:50 +0900

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: StorePage(),
+    );
+  }
+}
+
+class StorePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          children: [
+            Row(
+              children: [
+                Text("Woman"),
+                Text("Kids"),
+                Text("Shoes"),
+                Text("Bag"),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
